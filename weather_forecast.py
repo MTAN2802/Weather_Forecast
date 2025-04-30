64eba168c6421ff50c89ccce9336722519ddbad8
Marcus T <marcustan8998@gmail.com>
2025-04-30 20:07:21 +1000

    Created ETL Pipeline that takes in location, and how many days ahead to forecast. Creates weather_forecast.csv to show data, updates it if already exists

--- weather_forecast.py
+++ weather_forecast.py
@@ -17,19 +17,34 @@ def extract_forecast(location):
 
 
 #Transforming data
-def transform_data(data, days_ahead): #where days_ahead is how many days from current date to look at
-    dateToSee = data["days"][days_ahead]
-    location_weather = {
-        "name": data["resolvedAddress"],
-        "forecast_description": data["description"],
-        "date": dateToSee["datetime"],
-        "temp": dateToSee["temp"],
-        "max_temp": dateToSee["tempmax"],
-        "min_temp": dateToSee["tempmin"],
-        "feelslike": dateToSee["feelslike"],
-        "day_description": dateToSee["description"],
-        "preciptype": ",".join(dateToSee["preciptype"]),
-        "humidity": dateToSee["humidity"],
-        "uvindex": dateToSee["uvindex"]
-    }
-    return location_weather
+def transform_data(data, days_ahead): #where days_ahead is how many days in the future to forecast the weather, max 2 weeks
+    info_list = []
+    for i in range(days_ahead+1):
+        dateToSee = data["days"][i]
+        location_weather = {
+            "name": data["address"],
+            "date": dateToSee["datetime"],
+            "forecast_description": data["description"],
+            "temp": dateToSee["temp"],
+            "max_temp": dateToSee["tempmax"],
+            "min_temp": dateToSee["tempmin"],
+            "feelslike": dateToSee["feelslike"],
+            "preciptype": ",".join(dateToSee["preciptype"]) if isinstance(dateToSee.get("preciptype"), (list, tuple)) else "Normal",
+            "humidity": dateToSee["humidity"],
+            "uvindex": dateToSee["uvindex"]
+        }
+        info_list.append(location_weather)
+    return info_list
+
+#Loading data
+def load_data(data, filename):
+    df = pd.DataFrame(data)
+    df.to_csv(filename, index=False)
+
+#Run the ETL Pipeline altogether
+def run_etl(location, days_ahead=0):
+    extracted_data = extract_forecast(location)
+    transformed_data = transform_data(extracted_data, days_ahead)
+    load_data(transformed_data, "weather_forecast.csv")
+
+run_etl('Kogarah', 14)
